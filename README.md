03808de736c7358e8d5d46ade934ab55d996291d
thangleuet <18021155@vnu.edu.vn>
2024-09-18 16:06:24 +0700

    update

---
+++ README.md
@@ -0,0 +1,52 @@
+# Setting up a Python Script as a Windows Service
+
+## Environment Setup
+
+1. Create a virtual environment and activate it:
+    ```bash
+    python -m venv env
+    env\Scripts\activate
+    ```
+
+2. Install required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Update Configurations
+
+- Modify the `config.ini` file to set the correct path for the log file:
+    ```ini
+    data_path = [path_to_log_file]
+    ```
+
+## Setting up the Python Script as a Windows Service
+
+1. Download [nssm (Non-Sucking Service Manager)](https://nssm.cc/download) and place the executable in your project folder or add it to your system path.
+
+2. Open **Command Prompt** as Administrator and navigate to your project folder.
+
+3. Install the Python script as a Windows service:
+    ```bash
+    nssm.exe install bot_ea_service
+    ```
+
+4. Specify the Python executable path and your script's location during the service setup.
+
+5. Use the following commands to manage the service:
+
+    - Start the service:
+        ```bash
+        nssm.exe start bot_ea_service
+        ```
+
+    - Stop the service:
+        ```bash
+        nssm.exe stop bot_ea_service
+        ```
+
+    - Remove the service:
+        ```bash
+        nssm.exe remove bot_ea_service
+        ```
+
---
+++ main.py
@@ -0,0 +1,114 @@
+import threading
+import time
+import requests
+import os
+import configparser
+
+config = configparser.ConfigParser()
+path_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Đọc file cấu hình
+config.read(os.path.join(path_dir, 'config.ini'))
+current_dir = config.get('paths', 'data_path')
+time_loop = config.getint('time', 'time_loop')
+
+
+current_date = time.strftime("%Y%m%d")
+log_path_dir = os.path.join(current_dir, "log")
+if not os.path.exists(log_path_dir):
+    os.makedirs(log_path_dir)
+
+output_log_path = os.path.join(log_path_dir, f"{current_date}.log")
+
+# Theo dõi file .log để kiểm tra dòng mới
+def follow_log_file(output_log_path):
+    current_date = time.strftime("%Y%m%d")
+    file_path = os.path.join(current_dir, f"{current_date}.log")
+    while True:
+        try:
+            # Check file exists
+            log_to_file(output_log_path, f"Monitoring file: {file_path}")
+            file = open(file_path, 'r', encoding='utf-8')
+            file.seek(0, 2)  # Đặt con trỏ về cuối file
+            
+            while True:
+                # check file path 
+                current_date = time.strftime("%Y%m%d")
+                new_file_path = os.path.join(current_dir, f"{current_date}.log")
+    
+                if new_file_path != file_path:
+                    file.close()
+                    file_path = new_file_path
+                    file = open(file_path, 'r', encoding='utf-8')
+                    file.seek(0, 2)
+
+                    output_log_path = os.path.join(log_path_dir, f"{current_date}.log")
+                    log_to_file(output_log_path, f"New date file: {file_path}")
+                    time.sleep(time_loop)
+                    continue
+                
+                line = file.readline()
+                if not line:
+                    time.sleep(1) 
+                    continue
+                yield line
+        except Exception as e:
+            log_to_file(output_log_path, f"File not found: {file_path} - {e}")
+            time.sleep(time_loop)
+
+
+# Gửi tin nhắn tới Telegram
+def send_telegram_message(token, chat_id, message):
+    try:
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        data = {
+            'chat_id': chat_id,
+            'text': message
+        }
+        response = requests.post(url, data=data)
+        return response.status_code
+    except Exception as e:
+        log_to_file(output_log_path, f"Error sending message: {e}")
+
+
+# Ghi log vào file .txt
+def log_to_file(log_file_path, message):
+    try:
+        with open(log_file_path, 'a', encoding='utf-8') as log_file:
+            date_time = time.strftime("%Y-%m-%d %H:%M:%S")
+            log_file.write(f"[{date_time}] - {message}\n")
+    except Exception as e:
+        log_to_file(output_log_path, f"Error logging to file: {e}")
+
+
+# Hàm để theo dõi log và gửi thông báo
+def monitor_log_and_notify(token, chat_id, output_log_path):
+    try:
+        for line in follow_log_file(output_log_path):
+            if line.strip() == '\x00':
+                continue
+            # Gửi tin nhắn tới Telegram
+            send_telegram_message(token, chat_id, f"{line.strip()}")
+            # Ghi log ra file .txt
+            log_to_file(output_log_path, line.strip())
+    except Exception as e:
+        log_to_file(output_log_path, f"Error monitoring file: {e}")
+
+
+# Chạy ngầm
+def start_background_monitor(token, chat_id, output_log_path):
+    monitor_thread = threading.Thread(target=monitor_log_and_notify, args=(token, chat_id, output_log_path))
+    monitor_thread.daemon = True  # Đặt thread chạy ngầm
+    monitor_thread.start()
+
+
+# if __name__ == "__main__":
+telegram_token = "7541641643:AAHgl-WobHqrUak3fBTjerDsF_Ws3jriRw0"
+telegram_chat_id = "1899348741"
+
+
+# Bắt đầu tiến trình theo dõi log chạy ngầm
+start_background_monitor(telegram_token, telegram_chat_id, output_log_path)
+
+while True:
+    time.sleep(10)  # Giữ cho chương trình chính tiếp tục chạy ngầm
